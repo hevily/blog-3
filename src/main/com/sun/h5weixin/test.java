05962b26c5986e0846e175ca7785368dd30957b2
gao <805019254@qq.com>
2017-04-12 09:08:56 +0800

    11

--- src/main/com/sun/h5weixin/test.java
+++
@@ -1,7 +0,0 @@
-package main.com.sun.h5weixin;
-
-/**
- * Created by SUN on 2017/3/8.
- */
-public class test {
-}
--- src/main/test.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-/**
- * Created by SUN on 2017/3/8.
- */
-public class test {
-}
