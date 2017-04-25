7a80e5dc66f1bed10ee812893644e38f245b079a
gaochenghui <805019254@qq.com>
2017-04-26 00:11:11 +0800

    251

--- blogDesign/js/main.js
+++ blogDesign/js/main.js
@@ -2,12 +2,13 @@
  * Created by Administrator on 2017/3/21.
  */
 $(function () {
-    
-        
         //article，hover
         $('article').mouseover(function  () {
         	$('article h3').removeClass('articleHover');
         	$(this).find('h3').addClass('articleHover');
         })
-
+        //loginIn
+        $(".loginImg").click(function(){
+        	$(".loginList").fadeToggle();
+        })
 })
